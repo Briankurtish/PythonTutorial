54c0651c80416d1873e7f34606cb6e77aa86269d
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-07-11 16:29:45 +0300

    day 2 first commit

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -1,2 +1,5 @@
+#Comments 
+#Print Command 
+
 print("Hello world")
 print("Hello Cipher")
---
+++ Day 2/day2.py
@@ -0,0 +1,9 @@
+#Variable
+
+age = 18
+
+#Concatenation: combining 2 expressions(Strings) together
+#TypeCasting: Changing the datatype of a variable
+
+print("John is " + str(age) + " years old" )
+
