cddfe7f80e8af16dbc10f618466e63c94505ba09
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-07-11 16:52:32 +0300

    mini project 1

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -1,9 +1,18 @@
 #Variable
 
+
 age = 18
+name = "Cipher"
+address = "Magusa"
+school = "EMU"
+number_of_children = 3
 
 #Concatenation: combining 2 expressions(Strings) together
 #TypeCasting: Changing the datatype of a variable
 
-print("John is " + str(age) + " years old" )
+print("There was once a time")
+print("A man called " + name )
+print("Who lived in " + address)
+print("and schooled at " + school)
+print("He also had " + str(number_of_children) + " children")
 
