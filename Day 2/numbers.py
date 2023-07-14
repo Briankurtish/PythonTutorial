48e62185876eb67667b05bf71b766c1d09c22292
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-07-14 13:10:05 +0300

    done

---
+++ Day 2/numbers.py
@@ -0,0 +1,20 @@
+# Numbers 
+
+#Addition (+)
+
+# x = input("Enter the value of X: ")
+# y = input("Enter the value of Y: ")
+
+# answer = int(x) + int(y)
+
+# print("The Result of the Addition is: " + str(answer))
+
+
+# Conditionals
+
+age = input("Enter your Age: ")
+
+if (int(age) < 18):
+  print("You can not access the platform")
+elif(int(age) >= 18):
+  print("You can access the platform ") 
---
+++ Day 2/user_input.py
@@ -0,0 +1,7 @@
+# Working with user inputs 
+
+age = input("Enter your Age: ")
+print(age)
+
+
+print(type(age))
