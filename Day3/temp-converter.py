3359f3b913d1d73911fad77547daabf5c7eb2053
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-07-18 21:06:12 +0300

    update 5

--- Day3/temp-converter.py
+++ Day3/temp-converter.py
@@ -25,6 +25,7 @@ if(int(user_option) == 1):
     #Modifications made 2
     #Modifications made 3
     #Modifications made 4
+    #Modifications made 5
     
     #Printing Result
     print(temp_F + " F " + " is equivalent to " + str(result_F) + " C ")
